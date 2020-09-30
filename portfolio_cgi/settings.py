73a7420c3763074a6057da0ffd81f72fa7a0bee8
khava <havarg96@gmail.com>
2020-09-30 13:38:46 +0300

    make it better

--- portfolio_cgi/settings.py
+++ portfolio_cgi/settings.py
@@ -151,8 +151,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'discussion', 'static'),
-    os.path.join(BASE_DIR, 'moderator', 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
