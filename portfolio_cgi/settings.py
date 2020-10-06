07d69cc5aa59cb0b04204f7b9605a5342eed9ee1
khava <havarg96@gmail.com>
2020-10-06 15:22:39 +0300

    commit

--- portfolio_cgi/settings.py
+++ portfolio_cgi/settings.py
@@ -97,23 +97,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'portfolio_cgi',
-#         'USER': 'portfolio_cgi',
-#         'PASSWORD': 'portfolio_cgi',
-#         'HOST': '192.168.99.100',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio_cgi',
+        'USER': 'portfolio_cgi',
+        'PASSWORD': 'portfolio_cgi',
+        'HOST': 'portfolio-cgi.herokuapp.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
